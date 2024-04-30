b49487a19340ce8e55004dc048a0d715ccc9eb81
OlhaKatrych <olgak198931@gmail.com>
2024-04-30 20:15:38 +0300

    change title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import Product from "./Product/Product";
+import booksArr from "../favouriteBooks.json";
+import BookList from "./BookList/BookList";
+
 function App() {
+  const numArr = [1, 2, 3];
   return (
     <div>
       <p>Hello, baby!</p>
-      <Product title="The best girl!"/>
+      <Product title="The best Woman!" />
+      <h2>The books of the week</h2>
+      <BookList books={booksArr} />
     </div>
   );
 }
